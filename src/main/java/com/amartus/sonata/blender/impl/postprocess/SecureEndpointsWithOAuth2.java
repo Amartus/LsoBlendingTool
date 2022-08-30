2e01552f1e8f13b2830804f215321ac0f80cee3f
Bartosz Michalik <bartosz.michalik@gmail.com>
2022-08-30 13:42:00 +0200

    improved security definitions
    Signed-off-by: Bartosz Michalik <bartosz.michalik@gmail.com>

--- src/main/java/com/amartus/sonata/blender/impl/postprocess/SecureEndpointsWithOAuth2.java
+++ src/main/java/com/amartus/sonata/blender/impl/postprocess/SecureEndpointsWithOAuth2.java
@@ -18,39 +18,155 @@
 
 package com.amartus.sonata.blender.impl.postprocess;
 
+import com.amartus.sonata.blender.impl.util.Pair;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.security.*;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class SecureEndpointsWithOAuth2 implements Consumer<OpenAPI> {
+
+    private static final String DEFAULT_SCHEME_NAME = "oauth2MEFLSOAPI";
+    private final String schemeName;
+    private final Mode mode;
+
+    public SecureEndpointsWithOAuth2() {
+        this(DEFAULT_SCHEME_NAME, Mode.PER_OPERATION_SCOPE);
+    }
+
+    public SecureEndpointsWithOAuth2(String schemeName, Mode mode) {
+        this.schemeName = schemeName;
+        this.mode = mode;
+    }
+
     @Override
     public void accept(OpenAPI openAPI) {
-        var schemeName = "oauth2MEFLSOAPI";
-
-        openAPI.getComponents()
-                .addSecuritySchemes(
-                        schemeName, new SecurityScheme()
-                                .type(SecurityScheme.Type.OAUTH2)
-                                .description("Authorization code flow")
-                                .flows(
-                                        new OAuthFlows()
-                                                .authorizationCode(defaultAuthorizationCode("http://mef.net/example"))
-                                )
-                );
-
-        openAPI.addSecurityItem(new SecurityRequirement().addList(schemeName, "default"));
-    }
-
-    private OAuthFlow defaultAuthorizationCode(String url) {
-        if (url.endsWith("/")) {
-            url = url.substring(0, url.length() - 1);
+        SecurityModifier modifier;
+        if (mode == Mode.SINGLE_SCOPE) {
+            modifier = new DefaultSecurityModifier(openAPI);
+        } else {
+            modifier = new PerOperationSecurityModifier(openAPI);
         }
-        return new OAuthFlow()
-                .authorizationUrl(url + "/token")
-                .tokenUrl(url + "/refresh")
-                .scopes(new Scopes().addString(
-                        "default", "default scope "
-                ));
+
+        modifier.execute();
     }
+
+    private enum Mode {
+        PER_OPERATION_SCOPE,
+        SINGLE_SCOPE
+    }
+
+    private class DefaultSecurityModifier extends SecurityModifier {
+
+        private DefaultSecurityModifier(OpenAPI oas) {
+            super(oas);
+        }
+
+        @Override
+        protected void addRequirements(List<Pair<Operation, String>> operations) {
+            oas.addSecurityItem(
+                    new SecurityRequirement()
+                            .addList(schemeName, "default")
+            );
+        }
+
+        @Override
+        protected Scopes toScopes(List<Pair<Operation, String>> operations) {
+            var scopes = new Scopes();
+            scopes.addString("default", "default scope");
+            return scopes;
+        }
+    }
+
+    private class PerOperationSecurityModifier extends SecurityModifier {
+
+        private PerOperationSecurityModifier(OpenAPI oas) {
+            super(oas);
+        }
+
+        @Override
+        protected void addRequirements(List<Pair<Operation, String>> operations) {
+            operations.forEach(e -> {
+                var operation = e.first();
+                operation.addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(schemeName, e.second())
+                );
+            });
+        }
+
+        @Override
+        protected Scopes toScopes(List<Pair<Operation, String>> operations) {
+            var scopes = new Scopes();
+
+            operations.stream()
+                    .map(Pair::second)
+                    .forEach(name -> scopes.addString(name, String.format("Scope for operation %s", name)));
+            return scopes;
+        }
+    }
+
+    private abstract class SecurityModifier {
+        protected final OpenAPI oas;
+
+        private SecurityModifier(OpenAPI oas) {
+            this.oas = oas;
+        }
+
+        public void execute() {
+            var operations = oas.getPaths().entrySet().stream()
+                    .flatMap(e -> e.getValue().readOperations().stream().map(o -> Pair.of(o, operationId(e.getKey(), o))))
+                    .collect(Collectors.toList());
+
+            addSecuritySchema(operations);
+            addRequirements(operations);
+
+        }
+
+        protected abstract void addRequirements(List<Pair<Operation, String>> operations);
+
+        protected abstract Scopes toScopes(List<Pair<Operation, String>> operations);
+
+        private String operationId(String path, Operation operation) {
+            var id = operation.getOperationId();
+            if (id != null) {
+                return id;
+            }
+            return Base64.getEncoder()
+                    .encodeToString((operation + path).getBytes(StandardCharsets.UTF_8));
+        }
+
+        private void addSecuritySchema(List<Pair<Operation, String>> operations) {
+
+            oas.getComponents()
+                    .addSecuritySchemes(
+                            schemeName, new SecurityScheme()
+                                    .type(SecurityScheme.Type.OAUTH2)
+                                    .description("Default m2m client code")
+                                    .flows(
+                                            new OAuthFlows()
+                                                    .clientCredentials(flow("http://mef.net/example", toScopes(operations)))
+                                    )
+                    );
+
+        }
+
+        private OAuthFlow flow(String url, Scopes scopes) {
+            if (url.endsWith("/")) {
+                url = url.substring(0, url.length() - 1);
+            }
+            return new OAuthFlow()
+                    .authorizationUrl(url + "/token")
+                    .tokenUrl(url + "/refresh")
+                    .scopes(scopes);
+        }
+
+    }
+
+
 }
